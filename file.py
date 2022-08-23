23361dd2941d36b01d6f8c9c1340461b8cccd2cd
mohamed123 <chaabenim318@gmail.com>
2022-08-23 18:48:22 +0100

    update file

---
+++ file.py
@@ -0,0 +1,16 @@
+print("mohamed chaabani") # te5dem kima console.log()
+
+print("safe chaabani") ; print("yassine chaabani") # ken el stetment 3ala nafs el line lezem n7ot (;)
+
+type(5) # tala3li el type mta3 el données
+
+# el données el kol 3ibara 3ala object
+
+print(type(5)) # int => Integer
+print(type(5.5)) # float => Foating Point Number
+print(type('5')) # str => string
+print(type(True)) # bool => Boolean
+print(type([1 , 2 , 3])) # list => List
+print(type((1 , 2 , 3))) # tuple => Tuple
+print(type({ "name": "mohamed" , "age": 32 })) # dict => Dictionary
+
